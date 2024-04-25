2100a1aa5db77ae5c1234d18c8799f82b3d04a52
Kisly <92755737+KislyLemon4ik@users.noreply.github.com>
2024-04-26 02:13:55 +0300

    Added history bar

--- src/pages/main-page/ui/components/history/ui/History.vue
+++ src/pages/main-page/ui/components/history/ui/History.vue
@@ -1,5 +1,37 @@
+<script setup lang="ts">
+const history = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, ];
+</script>
+
 <template>
-	<div>
-		history
+	<div class="history-wrapper">
+		<div class="history-content" v-for="(item, index) in history" :key="index"> 
+			<div class="history-content-wrapper">
+				{{ item }}
+			</div>
+		</div>
 	</div>
 </template>
+
+<style scoped lang="scss">
+.history-wrapper
+{
+	display: flex;
+	flex-direction: column;
+	width: 400px;
+	border-right: 2px solid gray;
+	height: 100vh;
+	overflow-y: auto;
+}
+.history-content
+{
+	flex-shrink: 0;
+	height: 100px;
+	border-bottom: 1px solid gray;
+}
+.history-content-wrapper
+{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
