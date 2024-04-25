c2c50d9572b11ee1a7e22b5080d2f65bc5631610
MaximGaluga <91080628+MaximGaluga@users.noreply.github.com>
2024-04-25 23:40:53 +0300

    Added prototypes of two main components

--- src/pages/main-page/ui/MainPage.vue
+++ src/pages/main-page/ui/MainPage.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
+import History from './components/History.vue'
+import Chat from './components/Chat.vue'
 
 </script>
 
 <template>
-	<div>
-		
+	<div class="main-page-wrapper">
+		<History style="grid-area: history;" />
+		<Chat style="grid-area: chat;" />
 	</div>
 </template>
 
 <style scoped lang="scss">
-
+.main-page-wrapper
+{
+	display: grid;
+	grid-template:
+		'history chat'
+		/ auto 1fr
+}
 </style>
